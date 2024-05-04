578e4dff54c5ce4c1181cfa035a007dfab6988ef
Ravikira Yadava <ravikiranjobs21@gmail.com>
2024-05-03 23:07:16 -0400

    updated home page contents

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,10 @@
-
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <p>This is a good home page for your website.</p>
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
